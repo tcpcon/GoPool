c6e8437a18f055bb6ea06dabefcb113dddedc66d
tcpcon <89215892+tcpcon@users.noreply.github.com>
2023-04-13 17:10:06 +0100

    Update utils.go

--- utils.go
+++ utils.go
@@ -2,10 +2,29 @@ package gopool
 
 import "github.com/golang-collections/collections/stack"
 
-func transform[V any](slice []V) (stck stack.Stack) {
-	for i := len(slice) - 1; i >= 0; i-- {
-		stck.Push(slice[i])
+func transform[V comparable](slice []V) stack.Stack {
+	var (
+		new  []V
+		stck stack.Stack
+	)
+
+	for _, v1 := range slice {
+		if !func() bool {
+			for _, v2 := range slice {
+				if v1 == v2 {
+					return true
+				}
+			}
+
+			return false
+		}() {
+			new = append(new, v1)
+		}
 	}
 
-	return
+	for i := len(new) - 1; i >= 0; i-- {
+		stck.Push(new[i])
+	}
+
+	return stck
 }
