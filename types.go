58412d123b44b172026427634b739b69ea48f4ef
tcpcon <89215892+tcpcon@users.noreply.github.com>
2023-04-13 19:22:38 +0100

    Update types.go

--- types.go
+++ types.go
@@ -1,8 +1,15 @@
 package gopool
 
-type PoolerParams[V comparable] struct {
-	Slice                  []V
-	WorkerFn               func(V) error
-	ErrorFn                func(error, bool)
-	MaxRoutines, MaxErrors int
-}
+type (
+	PoolerParams[V comparable] struct {
+		Slice                  []V
+		WorkerFn               func(V) error
+		ErrorFn                func(error, bool)
+		MaxRoutines, MaxErrors int
+	}
+
+	Data struct {
+		uuid string
+		data any
+	}
+)
